b82836ac1d074020531e216b5d40f719809b84bb
malikimran-kh <malikimranawan3124@gmail.com>
2025-11-16 20:21:20 +0500

    add navbar and footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeMain from "./pages/home/HomeMain";
+
 export default function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<HomeMain/>}></Route>
+    </Routes>
+    </BrowserRouter>
     here is video app
     </>
   );
