41ec165251d5a057da299508d5747bd84b7fa96b
malikimran-kh <malikimranawan3124@gmail.com>
2026-04-12 23:01:21 +0500

    testing server

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import serverless from "serverless-http";
 import { app, server } from "./socket/socket.js";
-
+app.get("/api/test", (req, res) => {
+  res.send("API is running....");
+});
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -20,7 +22,12 @@ dotenv.config();
 connectDB();
 
 // Middleware
-// Robust CORS Middleware
+// Robust CORS Middleware 
+
+app.get("/api/test", (req, res) => {
+  res.send("API is running....");
+});
+
 const corsOptions = {
   origin: (origin, callback) => {
     callback(null, true);
@@ -29,12 +36,16 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 };
-
+app.get("/api/tests", (req, res) => {
+  res.send("API is running....");
+});
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" })); // For avatar base64
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.get("/api/testing", (req, res) => {
+  res.send("API is running....");
+});
 import groupRoutes from "./routes/groupRoutes.js";
 
 // Routes
